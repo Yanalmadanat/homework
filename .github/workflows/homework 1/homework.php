dbe5c0f32f09385752094a999305159096ec5bb4
Yanalmadanat <96653105+Yanalmadanat@users.noreply.github.com>
2022-02-05 18:12:05 +0200

    Add files via upload

---
+++ .github/workflows/homework 1/homework.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<?php
+$statement = "Hello PHP";
+echo $statement."<br>" ;
+$firstVariable = "Welcome";
+$secondVariable = "to my page";
+echo $firstVariable."   ".$secondVariable;
+$myTitle = "HTML CODE";
+echo "<h1>{$myTitle}<h1>"."<br>"."<p>another title</p>";
+?>
